c8dd2669e66ddec95ffe8a0a8404d4826fb60999
Danilo Paggi <danilopaggi@gmail.com>
2020-03-21 12:35:33 -0300

    Exclusion of reactotron

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import GlobalStyle from "./styles/global";
 import Main from "./pages/Main";
-import "./config/reactotron";
 import "./config/toastify";
 
 function App() {
