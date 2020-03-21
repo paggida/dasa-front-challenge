79880d2bc9e9fe40a5a626c00229840fcd3fd956
Danilo Paggi <danilopaggi@gmail.com>
2020-03-21 08:00:28 -0300

    Add the reactotron support and toastify feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from "react";
 import GlobalStyle from "./styles/global";
 import Main from "./pages/Main";
+import "./config/reactotron";
+import "./config/toastify";
 
 function App() {
   return (
